5e3af0330944dbb205182ba1ebb724544e5d85bf
Shraddha D. Sankpal <shraddhasankpal439@gmail.com>
2026-08-12 16:42:33 +0530

    assignment no 3

---
+++ assignment_no_3/que13.py
@@ -0,0 +1,20 @@
+units = float(input("Enter electricity units: "))
+
+if units <= 50:
+    bill = units * 0.50
+
+elif units <= 150:
+    bill = (50 * 0.50) + ((units - 50) * 0.75)
+
+elif units <= 250:
+    bill = (50 * 0.50) + (100 * 0.75) + ((units - 150) * 1.20)
+
+else:
+    bill = (50 * 0.50) + (100 * 0.75) + (100 * 1.20) + ((units - 250) * 1.50)
+
+surcharge = bill * 20 / 100
+total_bill = bill + surcharge
+
+print("Electricity Bill =", bill)
+print("Surcharge =", surcharge)
+print("Total Bill =", total_bill)
---
+++ assignment_no_3/que7.py
@@ -0,0 +1,7 @@
+userid = input("Enter User ID: ")
+password = input("Enter Password: ")
+
+if userid == "admin" and password == "1234":
+    print("Correct User ID and Password")
+else:
+    print("Incorrect User ID or Password")
