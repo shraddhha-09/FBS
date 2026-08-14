29f07fd2ca65197fb82a9f331911287f7776f175
Shraddha D. Sankpal <shraddhasankpal439@gmail.com>
2026-08-14 18:07:16 +0530

    Test1

---
+++ Test1/Q2.py
@@ -0,0 +1,6 @@
+P=int(input("Enter Priciple:"))
+R=float(input("Enter Rate:"))
+T=float(input("Enter Time:"))
+
+SI=((P*R*T)/100)
+print('Simple Intrest:',SI)
---
+++ Test1/Q3.py
@@ -0,0 +1,7 @@
+km=int(input("Enter distance in Kilometer:"))
+
+M=km*1000
+CM=100000
+
+print("Distance in Metere:",M)
+print("Distnace in Centimeter",CM)
